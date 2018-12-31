027fa7ba2087a1a5c38f410c3aec7e6bb72efd6c
DaliaCode <46163355+DaliaCode@users.noreply.github.com>
2018-12-31 15:44:15 +0100

    Update fibonacci_python.py
    Compute the sequence of the fibonacci numbers until a given number n using the recursive approach.

--- fibonacci_python.py
+++ fibonacci_python.py
@@ -3,3 +3,9 @@ def nth_fibonacci(n):
 		return n
 	else:
 		return nth_fibonacci(n-1)+nth_fibonacci(n-2)
+
+def fibonacci_until_n(n):
+	i=0
+	while (nth_fibonacci(i)<n):
+		print(nth_fibonacci(i))
+		i+=1
